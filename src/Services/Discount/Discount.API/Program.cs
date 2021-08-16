723964b657da3168aabb073626a8bf5f4ace097c
Walt Holliday <wholliday@prometheusgroup.com>
2021-08-16 06:07:50 -0400

    Containerized Discount.API. Dynamically instantiated database table for discounts. Modified Program.cs file to handle generation of database table.

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Discount.API.Extensions;
 
 namespace Discount.API
 {
@@ -13,14 +8,25 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+
+            // The following was the original code
+            // ======================================
+            // CreateHostBuilder(args).Build().Run();
+
+            var host = CreateHostBuilder(args).Build();
+            // Reference: Discount.API.Extensions
+            host.MigrateDatabase<Program>();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                //.UseSerilog(SeriLogger.Configure) // Added for Serilog
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });    
+
+      
     }
 }
