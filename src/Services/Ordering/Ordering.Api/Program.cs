3659f0314bb9970dfc2b2f48ab82ce12f55e7402
WeAreUSS <Walt@WeAreUSS.com>
2021-09-26 08:53:23 -0400

    Addewd comments.

--- src/Services/Ordering/Ordering.Api/Program.cs
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -32,6 +32,21 @@ namespace Ordering.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    // The below is an optional way to configure KestrelListners
+                    // When you set the URLs for kestrel in this way,
+                    // it overrides the URLS configuration value if you've set it through one of the other mechanisms as well,
+                    // such as environment variables.
+                    //==========================================================
+                    //webBuilder.UseKestrel(opts =>
+                    //{
+                    //    // Bind directly to a socket handle or Unix socket
+                    //    // opts.ListenHandle(123554);
+                    //    // opts.ListenUnixSocket("/tmp/kestrel-test.sock");
+                    //    opts.Listen(IPAddress.Loopback, port: 5002);
+                    //    opts.ListenAnyIP(5003);
+                    //    opts.ListenLocalhost(5004, opts => opts.UseHttps());
+                    //    opts.ListenLocalhost(5005, opts => opts.UseHttps());
+                    //});
                 });
     }
 }
